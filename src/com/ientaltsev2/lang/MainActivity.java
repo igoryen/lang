f6509579fbcdc5fd457d248358dd8383ca010e49
igoryen <igor.yentaltsev@gmail.com>
2014-04-12 13:53:57 -0400

    Beginning: Removed some blocks of code

--- src/com/ientaltsev2/lang/MainActivity.java
+++ src/com/ientaltsev2/lang/MainActivity.java
@@ -1,10 +1,66 @@
 package com.ientaltsev2.lang;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	// 200
+
+	// 300
+
+	// 400
+
+	// 500
+
+	// 600
+
+	// 700
+
+	// 800
+
+
+
+
+	// ====================================================================
+	// Create a subclass of AsyncTask to:
+	// 10. call the WordDefinition() a-synchronously
+	// ====================================================================
+	private class AccessWebServiceTask extends AsyncTask<String, Void, String> {
+		protected String doInBackground(String... urls) {
+			return WordDefinition(urls[0]); // 10
+		}
+
+		protected void onPostExecute(String result) {
+			Toast.makeText(getBaseContext(), result, Toast.LENGTH_LONG).show();
+		}
+	}
+
+
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,4 +75,4 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-}
+} // MainActivity
